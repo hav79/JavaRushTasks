f05273d5f8d0f05d8241b1e0f96ecf3b19fb4e47
hav79 <abambr@gmail.com>
2018-03-13 11:44:36 +0300

    Java Multithreading Level 3 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Inner
+*/
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution[] solutions = new Solution[2];
+        for (int i = 0; i < 2; i++) {
+            Solution solution = new Solution();
+            solution.innerClasses[0] = solution.new InnerClass();
+            solution.innerClasses[1] = solution.new InnerClass();
+            solutions[i] = solution;
+        }
+
+        return solutions;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
